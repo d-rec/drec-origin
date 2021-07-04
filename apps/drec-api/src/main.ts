15cd9fb8ca7623235a8c71e3044fa65c264d31da
Malak67 <florin.georgescu@energyweb.org>
2021-07-04 20:17:57 +0300

    API working

--- apps/drec-api/src/main.ts
+++ apps/drec-api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DrecModule } from './drec.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(DrecModule);
   await app.listen(3000);
 }
 bootstrap();
