ea1f1a11fd0a9d61c4c07f7c383754aa277b730b
Malak67 <flo_georgescu@hotmail.com>
2021-07-14 08:30:02 +0300

    Implemented file endpoints

---
+++ apps/drec-api/src/pods/file/file.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { FileController } from './file.controller';
+import { File } from './file.entity';
+import { FileService } from './file.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([File])],
+  providers: [FileService],
+  controllers: [FileController],
+  exports: [FileService],
+})
+export class FileModule {}
