e289e36e8f7f5a68d964267ac291808c24c80b27
Malak67 <flo_georgescu@hotmail.com>
2021-09-03 21:15:49 +0300

    chore(drec-ui) removing smart meeter table for now

--- apps/drec-ui/src/device/containers/smartMeter/SmartMeterBlock/SmartMeterBlock.tsx
+++ apps/drec-ui/src/device/containers/smartMeter/SmartMeterBlock/SmartMeterBlock.tsx
@@ -1,7 +1,6 @@
 import { Grid, Paper } from '@material-ui/core';
 import React, { FC } from 'react';
 import { SmartMeterChart } from '../SmartMeterChart';
-import { SmartMeterTable } from '../SmartMeterTable';
 import { useStyles } from './SmartMeterBlock.styles';
 import { DeviceDTO } from '@energyweb/origin-drec-api-client';
 
@@ -14,11 +13,7 @@ export const SmartMeterBlock: FC<SmartMeterBlockProps> = ({ device }) => {
     return (
         <Paper className={classes.paper}>
             <Grid container spacing={4}>
-                <Grid item xs={12} md={4}>
-                    <SmartMeterTable device={device} />
-                </Grid>
-
-                <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={12}>
                     <SmartMeterChart meterId={device.id} />
                 </Grid>
             </Grid>
