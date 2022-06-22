4eaf6e0e5dbf930a535685e84c849b46a2f99254
Namrata <nsonkar@sweyainfotech.com>
2022-06-22 19:16:05 +0530

    orgzanization_table update

---
+++ apps/drec-api/migrations/1655904568851-OrganizationUpdatingdropschema.ts
@@ -0,0 +1,23 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class OrganizationUpdatingdropschema1655904568851 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "organization" ADD "orgEmail" character varying NOT NULL`,
+          );
+          await queryRunner.query(
+            `ALTER TABLE "organization" ADD "organizationType"  character varying NOT NULL`,
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryFullName"`);
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryAddress"`);
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryZipCode"`);
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryCity"`);
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryCountry"`);
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryPhoneNumber"`);
+    }
+
+}
