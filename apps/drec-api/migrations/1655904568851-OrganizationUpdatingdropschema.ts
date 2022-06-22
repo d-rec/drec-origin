393296c1aed2385c76dfe454e765fb5b3590e0b1
Namrata <nsonkar@sweyainfotech.com>
2022-06-22 19:31:21 +0530

    newdrop update

--- apps/drec-api/migrations/1655904568851-OrganizationUpdatingdropschema.ts
+++ apps/drec-api/migrations/1655904568851-OrganizationUpdatingdropschema.ts
@@ -1,24 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class OrganizationUpdatingdropschema1655904568851 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "organization" ADD "orgEmail" character varying NOT NULL`,
-          );
-          await queryRunner.query(
+        );
+        await queryRunner.query(
             `ALTER TABLE "organization" ADD "organizationType"  character varying NOT NULL`,
-          );
-    }
+        );
+        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryFullName", DROP COLUMN "signatoryAddress", DROP COLUMN "signatoryZipCode",DROP COLUMN "signatoryCity",DROP COLUMN "signatoryCountry",DROP COLUMN "signatoryPhoneNumber",DROP COLUMN "signatoryEmail"`
+        );
 
+    }
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryFullName"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryAddress"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryZipCode"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryCity"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryCountry"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryPhoneNumber"`);
-        await queryRunner.query(`ALTER TABLE "organization" DROP COLUMN "signatoryEmail"`);
+
     }
 
 }
