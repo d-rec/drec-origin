bae39e6cbbf8b57019b6807655e3194576ac4fa2
Namrata <nsonkar@sweyainfotech.com>
2022-08-11 16:19:50 +0530

    add enum

---
+++ apps/drec-api/src/utils/enums/read-type.enum.ts
@@ -0,0 +1,12 @@
+export enum ReadType {
+    History = 'History',
+    Delta = 'Delta',
+    ReadMeter = 'Aggregate',
+   
+  }
+
+  export enum ReadStatus{
+    Done='Done',
+    Pending= 'Pending'
+
+  }
