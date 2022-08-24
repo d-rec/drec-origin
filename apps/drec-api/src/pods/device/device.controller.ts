d20963a381f2bffe1c1168f05774060d8c8053fc
Suhail Ahmed <sahmed@sweyainfotech.com>
2022-08-24 13:08:42 +0530

    suhail - added log

--- apps/drec-api/src/pods/device/device.controller.ts
+++ apps/drec-api/src/pods/device/device.controller.ts
@@ -189,6 +189,7 @@ export class DeviceController {
             );
           });
         } else {
+          console.log("error",error);
           return new Promise((resolve, reject) => {
             reject({ error: true });
           });
