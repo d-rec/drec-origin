982b076c606c2e3377b09ca060fa4af41f359ab8
Namrata <nsonkar@sweyainfotech.com>
2022-09-16 10:38:48 +0530

    migration update

--- apps/drec-api/migrations/1663219743369-DeviceGroupNewIdField.ts
+++ apps/drec-api/migrations/1663219743369-DeviceGroupNewIdField.ts
@@ -4,7 +4,9 @@ export class DeviceGroupNewIdField1663219743369 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "device_group" 
-        ADD "devicegroup_uid" uuid NOT NULL DEFAULT uuid_generate_v4(),
+        ADD "devicegroup_uid" uuid NOT NULL DEFAULT uuid_generate_v4()
+        `)
+        await queryRunner.query(`ALTER TABLE "check_certificate_issue_date_log_for_device_group" 
         ADD  "countryCode" character varying
         `)
     }
