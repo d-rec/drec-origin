33809ccf6230c1496f9e7d30786e407e896b3a5f
Suhail Ahmed <sahmed@sweyainfotech.com>
2022-09-29 19:08:34 +0530

    suhail - added api call

--- apps/drec-ui/src/apps/sample/view/pages/TableView/tableViewConfig.tsx
+++ apps/drec-ui/src/apps/sample/view/pages/TableView/tableViewConfig.tsx
@@ -1,13 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC,useState, useEffect } from 'react';
 import { useStyles } from './tableViewConfig.style';
 import { Paper } from '@mui/material';
+
+import axios from 'axios';
 // import { GenericForm } from '@energyweb/origin-ui-core';
 // //import { usesamplePageEffects } from './FromViewConfig.effect';
 
 export const AddSampleListViewPage: FC = () => {
     const classes = useStyles();
+
+    const [keywords, setKeywords] = useState<any>()
+  const [fetchedData, setFetchedData] = useState<any>()
+
+  let fetchData = async ()=> {
+    const  data  = await axios.get(
+      'http://127.0.0.1:3040/api/device-group'
+    )
+    setFetchedData(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    fetchData()
+  }
+
+
     return (
         <Paper className={classes.paper}>
+
+        <div className="input-field">
+          <input
+            placeholder="Search whatever you wish"
+            type="text"
+            value={keywords}
+            onChange={e => setKeywords(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit}> Click Here TO make API Call</button>
+
             <table>
                 <tr>
                     <td>Cell 1</td>
