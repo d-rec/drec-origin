74a633d79a311b011f7e07e0bb1492046cf0321d
Namrata <nsonkar@sweyainfotech.com>
2022-10-03 16:40:22 +0530

    new sample form

--- apps/drec-ui/src/apps/sample/view/pages/AddFromView/FromViewConfig.tsx
+++ apps/drec-ui/src/apps/sample/view/pages/AddFromView/FromViewConfig.tsx
@@ -1,14 +1,16 @@
  import React, { FC } from 'react';
 import { useStyles } from './FromViewConfig.style';
  import { Paper } from '@mui/material';
-// import { GenericForm } from '@energyweb/origin-ui-core';
-// //import { usesamplePageEffects } from './FromViewConfig.effect';
+import { GenericForm } from '@energyweb/origin-ui-core';
+import { usesamplePageEffects } from './FromViewConfig.effect';
 
 export const AddSampleformValuePage: FC = () => {
    const classes= useStyles();
+   const { formConfig } = usesamplePageEffects();
+   
     return (
         <Paper className={classes.paper}>
-            <h1>hellohjjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h1>
+             <GenericForm {...formConfig} />
         </Paper> 
        
     );
