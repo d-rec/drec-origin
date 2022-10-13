bcf689fddc5315e3b9154395fdf85dd0334fcbbb
Namrata <nsonkar@sweyainfotech.com>
2022-10-13 17:44:32 +0530

    new commit

--- apps/drec-api/src/pods/device-group/device-group.service.ts
+++ apps/drec-api/src/pods/device-group/device-group.service.ts
@@ -363,7 +363,7 @@ export class DeviceGroupService {
     await Promise.all(
       devices.map(async (device: Device) => {
         console.log(typeof device.createdAt )
-        if( data.reservationStartDate< device.createdAt){
+        if( data.reservationStartDate.getTime()< device.createdAt.getTime()){
           const nexthistorydevicecrtifecateissue = await this.historynextissuancedaterepository.save({
           
             groupId: group.id,
