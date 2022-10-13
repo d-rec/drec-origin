1033e79801f40711af059d845cbff2ff8b4921f6
Namrata <nsonkar@sweyainfotech.com>
2022-10-13 18:02:24 +0530

    enumhistory

--- apps/drec-api/src/utils/enums/index.ts
+++ apps/drec-api/src/utils/enums/index.ts
@@ -15,4 +15,5 @@ export * from './integrator.enum';
 export * from './yieldstatus.enum';
 export * from './read-type.enum';
 export * from './certificate-type.enum';
+export * from './history_next_issuance.enum'
 
