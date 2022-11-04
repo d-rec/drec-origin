b2fbfadd84a1c5941b22cf1605d256208bfe9642
Namrata <nsonkar@sweyainfotech.com>
2022-11-04 10:31:59 +0530

    add new api in certificatelog

---
+++ apps/drec-api/src/pods/certificate-log/dto/certificatewithdevicelog.dto.ts
@@ -0,0 +1,14 @@
+
+import { Certificate } from '@energyweb/issuer-api';
+import { CheckCertificateIssueDateLogForDeviceEntity } from '../../device/check_certificate_issue_date_log_for_device.entity'
+
+export class CertificateWithPerdevicelog extends Certificate {
+    id: number;
+    deviceId: string;
+    generationStartTime: number;
+    generationEndTime: number;
+    creationTime: number;
+    perDeviceCertificateLog: CheckCertificateIssueDateLogForDeviceEntity[];
+    metadata:string;
+    
+}
