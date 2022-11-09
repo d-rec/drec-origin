1705b0d378106ae1d0a684ff24a4dea85c17ce28
Suhail Ahmed <sahmed@sweyainfotech.com>
2022-11-09 13:49:43 +0530

    suhail - added utils check iso file

---
+++ apps/drec-api/src/utils/checkForISOStringFormat.ts
@@ -0,0 +1,10 @@
+import moment from 'moment';
+
+export const isValidUTCDateFormat =(dateInput:string) =>{
+    let dateFormateToCheck = new RegExp(/\d\d\d\d\-\d\d\-\d\dT\d\d:\d\d:\d\d\.\d{1,}Z/);
+      ;
+    const dateFormat = 'YYYY-MM-DDTHH:mm:ssZ';
+    const momentDateFormatToCheck = moment(dateInput).format(dateFormat);
+
+    return dateFormateToCheck.test(dateInput) && moment(momentDateFormatToCheck, dateFormat, true).isValid();
+}
