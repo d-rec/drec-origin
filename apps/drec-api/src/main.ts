693855a6d138a5a304999ba7c53f6c95e6a20fa9
Suhail Ahmed <sahmed@sweyainfotech.com>
2022-11-25 16:27:35 +0530

    suhail - updated comment

--- apps/drec-api/src/main.ts
+++ apps/drec-api/src/main.ts
@@ -1,3 +1,14 @@
 import { startAPI } from '.';
 
+//@ts-ignore
+process.setMaxListeners(0);
+
+/*
+https://stackoverflow.com/questions/57115918/maxlistenersexceededwarning-possible-eventemitter-memory-leak-dete
+
+(node:1) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 global:completed listeners added to [Queue]. Use emitter.setMaxListeners() to increase limit
+
+got this in log when trying to issue 50 certificates
+*/
+
 startAPI();
