05abae8d179a5a4f5b9ce55fc41750981b1fcad6
Suhail Ahmed <sahmed@sweyainfotech.com>
2022-12-12 16:20:24 +0530

    suhail - updated type

--- apps/drec-api/src/pods/file/file-upload.dto.ts
+++ apps/drec-api/src/pods/file/file-upload.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class FileUploadDto {
-  @ApiProperty({ type: 'string', format: 'binary', isArray: true })
+  @ApiProperty({ type: 'blob', format: 'binary', isArray: true })
   files: Blob[];
   type?:string;
 }
