643ee209faa851bf139aa2d856a9300fc3e461f6
NamrataSweya <102014274+NamrataSweya@users.noreply.github.com>
2022-12-27 16:20:03 +0530

    dainamic add api url

--- apps/drec-angular-ui/src/app/auth/authbase.service.ts
+++ apps/drec-angular-ui/src/app/auth/authbase.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import {environment} from '../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthbaseService {
-  url: String = 'http://localhost:3040/api/';
+  url: String = environment.API_URL;
   constructor(private httpClient:HttpClient) { }
 
 //   lohinhttpOptions() {
