b369407baf365bbb9ac57ae0850ae251246c4801
malathikapu <117283437+malathikapu@users.noreply.github.com>
2023-01-12 11:47:01 +0530

    test

--- apps/drec-angular-ui/src/app/app.module.ts
+++ apps/drec-angular-ui/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { CertificateDetailsComponent } from './view/certificate-details/certific
 import { WithloginlayoutComponent } from './nav/withloginlayout/withloginlayout.component';
 import { WithoutloginlayoutComponent } from './nav/withoutloginlayout/withoutloginlayout.component';
 import { AddDevicesComponent } from './view/add-devices/add-devices.component';
+import { FormFieldComponent } from './form-field/form-field.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +36,7 @@ import { AddDevicesComponent } from './view/add-devices/add-devices.component';
     WithloginlayoutComponent,
     WithoutloginlayoutComponent,
     AddDevicesComponent,
+    FormFieldComponent,
     
   ],
   imports: [
--- apps/drec-angular-ui/src/app/auth/authbase.service.ts
+++ apps/drec-angular-ui/src/app/auth/authbase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-//import {environment} from '../../environments/environment.dev';
-import { environment } from '../../environments/environment';
+import {environment} from '../../environments/environment.dev';
+// import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
