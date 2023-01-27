2a300f009fee31d511651be87615e325e71204d9
Ahmed-Suhail <sahmed@sweyainfotech.com>
2023-01-27 11:46:12 +0530

    suahil - updated claim

--- apps/drec-angular-ui/src/app/auth/services/blockchain-drec.service.ts
+++ apps/drec-angular-ui/src/app/auth/services/blockchain-drec.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { BlockchainProperties } from '../models/blockchain-properties.model';
-import { OrganizationInformation } from '../models/organization.model';
+import { environment } from '../../../environments/environment';
+import { OrganizationInformation } from '../../models/organization.model';
+import { BlockchainProperties } from '../../models/blockchain-properties.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +18,10 @@ export class BlockchainDrecService {
   {
     return this.httpClient.get<BlockchainProperties>(environment.API_URL+'blockchain-properties')
   }
+
+  convertClaimAmountToHex(amount:number|string):Observable<any>
+  {
+    return this.httpClient.get<any>(environment.API_URL+`certificate-log/claim-amount-in-ethers-json?amount=${amount.toString()}`)
+  }
+  
 }
