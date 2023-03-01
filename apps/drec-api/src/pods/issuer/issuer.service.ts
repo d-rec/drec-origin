18658305cf2c5223afa830c0e2dcb720c48bdd52
NamrataSweya <nsonkar@sweyainfotech.com>
2023-03-01 11:04:42 +0530

    new get meter read api add in response history and ongoing

--- apps/drec-api/src/pods/issuer/issuer.service.ts
+++ apps/drec-api/src/pods/issuer/issuer.service.ts
@@ -102,7 +102,7 @@ export class IssuerService {
     });
   }
 
- // @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async handleCron(): Promise<void> {
     this.logger.debug('Called every 10 minutes to check for isssuance of certificates');
 
@@ -222,7 +222,7 @@ export class IssuerService {
 
 
 
- // @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async handleCronForHistoricalIssuance(): Promise<void> {
     const historydevicerequestall = await this.groupService.getNextHistoryissuanceDevicelog();
     console.log(historydevicerequestall);
