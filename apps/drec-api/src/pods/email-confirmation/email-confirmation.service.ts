63c891212717d992ec4e98f79c0f2dc7837722ee
Suhail Ahmed <sahmed@sweyainfotech.com>
2023-03-24 16:43:16 +0530

    suhail - updated query

--- apps/drec-api/src/pods/email-confirmation/email-confirmation.service.ts
+++ apps/drec-api/src/pods/email-confirmation/email-confirmation.service.ts
@@ -27,7 +27,10 @@ export class EmailConfirmationService {
 
   public async create(user: User,inviteuser?:Boolean): Promise<EmailConfirmation> {
     const exists = await this.repository.findOne({
-      user: { email: user.email },
+      where:{
+        user: { email: user.email }
+      },
+      relations:['user']
     });
 
     if (exists) {
