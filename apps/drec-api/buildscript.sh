315d2923d4a8faea281ae058ef8254db81af13f7
manji23 <manjeethreddy23@gmail.com>
2023-07-13 11:55:51 +0530

    updates in buildscript and buildspec

--- apps/drec-api/buildscript.sh
+++ apps/drec-api/buildscript.sh
@@ -20,6 +20,15 @@ echo "Deploying $IMAGE - $BUILD_NUMBER - $DOCKERFILE"
 
 #exit 10
 
+npm i -g @microsoft/rush
+npm i -g pnpm
+cd ./apps/drec-api
+npm install --legacy-peer-deps
+npm run build -- --configuration=$environment
+rm -rf ./deployment
+mkdir ./deployment
+rush deploy -p @energyweb/origin-drec-api -t ./deployment --overwrite
+
 docker build -t $IMAGE:$BUILD_NUMBER -f $DOCKERFILE ./deployment
 
 aws ecr get-login-password --region eu-west-1  | docker login --username AWS --password-stdin 895706603967.dkr.ecr.eu-west-1.amazonaws.com
