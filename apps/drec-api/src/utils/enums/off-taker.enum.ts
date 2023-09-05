8c9c4321a49c3d9f8b891a55fab5586a82242ba0
NamrataSweya <nsonkar@sweyainfotech.com>
2023-09-05 16:44:24 +0530

    add new offtaker value in enum

--- apps/drec-api/src/utils/enums/off-taker.enum.ts
+++ apps/drec-api/src/utils/enums/off-taker.enum.ts
@@ -1,9 +1,13 @@
 export enum OffTaker {
   School = 'School',
+  Education="Education",
   HealthFacility = 'Health Facility',
   Residential = 'Residential',
   Commercial = 'Commercial',
   Industrial = 'Industrial',
   PublicSector = 'Public Sector',
-  Agriculture='Agriculture'
+  Agriculture='Agriculture',
+  Utility='Utility',
+  OffGridCommunity='Off-Grid Community'
+
 }
