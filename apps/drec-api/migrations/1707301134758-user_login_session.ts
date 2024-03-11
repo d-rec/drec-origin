7b85d9a81e5809847fb63fbdf8d3390828a26e61
Daniel Mohns <daniel.mohns@posteo.de>
2024-03-11 12:58:01 +0100

    format: Apply Prettier fix

--- apps/drec-api/migrations/1707301134758-user_login_session.ts
+++ apps/drec-api/migrations/1707301134758-user_login_session.ts
@@ -1,9 +1,9 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class userLoginSession1707301134758 implements MigrationInterface {
-    name = 'userLoginSession1707301134758';
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  name = 'userLoginSession1707301134758';
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
         CREATE TABLE user_login_session (
             id SERIAL PRIMARY KEY,
             "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
@@ -13,9 +13,7 @@ export class userLoginSession1707301134758 implements MigrationInterface {
 
            )
           `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
