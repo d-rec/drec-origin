8dec80c4fcdccab292b39e7b5175e551c09a8f06
NamrataSweya <nsonkar@sweyainfotech.com>
2024-03-18 18:05:28 +0530

    Migration files are broken and incomplete, added validation checker

---
+++ apps/drec-api/migrations/1710419707891-querfail_error.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { checkColumnQuoting } from '../src/utils/get-migration-unquotedColumnQuery'
+
+export class querfailError1710419707891 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+
+    const query = `
+      CREATE TABLE api_user_test (
+        "api_user_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+        "permissionIds" character varying,
+        Permission_status character varying DEFAULT 'Request'
+      )`;
+    try {
+      const unquotedColumnNames2 = await checkColumnQuoting(query);
+
+      if (!unquotedColumnNames2) {
+        throw new Error(`Missing double quotes in column names`);
+      }
+      await queryRunner.query(query);
+      console.log("Table created successfully.");
+    } catch (error) {
+      // Rollback transaction if an error occurs
+      await queryRunner.rollbackTransaction();
+      console.error(error.message);
+      throw error; // rethrow the error to mark the migration as failed
+    }
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Implement down migration if needed
+  }
+
+
+}
