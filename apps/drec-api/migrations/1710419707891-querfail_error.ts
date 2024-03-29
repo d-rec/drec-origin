c0bc006dd0d5524aa83c7742f990c78bf86338fa
NamrataSweya <nsonkar@sweyainfotech.com>
2024-03-29 09:16:04 +0530

    Migration files are broken and incomplete fixed prettier check issue

--- apps/drec-api/migrations/1710419707891-querfail_error.ts
+++ apps/drec-api/migrations/1710419707891-querfail_error.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-import { checkColumnQuoting } from '../src/utils/get-migration-unquotedColumnQuery'
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { checkColumnQuoting } from '../src/utils/get-migration-unquotedColumnQuery';
 
 export class querfailError1710419707891 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-
     const query = `
       CREATE TABLE api_user_test (
         "api_user_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
         "permissionIds" character varying,
-        Permission_status character varying DEFAULT 'Request'
+        "Permission_status" character varying DEFAULT 'Request'
       )`;
     try {
       const unquotedColumnNames2 = await checkColumnQuoting(query);
@@ -17,18 +16,13 @@ export class querfailError1710419707891 implements MigrationInterface {
         throw new Error(`Missing double quotes in column names`);
       }
       await queryRunner.query(query);
-      console.log("Table created successfully.");
+      console.log('Table created successfully.');
     } catch (error) {
       await queryRunner.rollbackTransaction();
       console.error(error.message);
-      throw error; 
+      throw error;
     }
-
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    
-  }
-
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
--- apps/drec-api/src/utils/get-migration-unquotedColumnQuery.ts
+++ apps/drec-api/src/utils/get-migration-unquotedColumnQuery.ts
@@ -12,19 +12,28 @@ export async function checkColumnQuoting(query): Promise<boolean> {
     closeParenIndex++;
   }
 
-  const columnsPart = query.substring(openParenIndex + 1, closeParenIndex - 1).trim();
-  const columnNames = columnsPart.split(',').map(column => column.trim());
-  const unquotedColumnNames = columnNames.filter(column => !column.startsWith('"') && !column.endsWith('"'));
+  const columnsPart = query
+    .substring(openParenIndex + 1, closeParenIndex - 1)
+    .trim();
+  const columnNames = columnsPart.split(',').map((column) => column.trim());
+  const unquotedColumnNames = columnNames.filter(
+    (column) => !column.startsWith('"') && !column.endsWith('"'),
+  );
   return unquotedColumnNames.length === 0;
 }
 
-export async function checkAlterColumnQuoting(query):Promise<boolean>{
-  const matches = query.match(/ALTER\s+TABLE\s+(?:\w+\.)?(\w+)\s+(ADD|ALTER|DROP)\s+(?:COLUMN\s+)?([^,;]+)/i);
+export async function checkAlterColumnQuoting(query): Promise<boolean> {
+  const matches = query.match(
+    /ALTER\s+TABLE\s+(?:\w+\.)?(\w+)\s+(ADD|ALTER|DROP)\s+(?:COLUMN\s+)?([^,;]+)/i,
+  );
   if (matches && matches.length >= 4) {
-      const [, tableName, action, columnDefinitions] = matches;
-      const columnNames = columnDefinitions.split(',').map(column => column.trim().split(' ')[0]);
-      const unquotedColumnNames = columnNames.filter(name => !name.startsWith('"') && !name.endsWith('"'));
-      return unquotedColumnNames.length === 0;
+    const [, tableName, action, columnDefinitions] = matches;
+    const columnNames = columnDefinitions
+      .split(',')
+      .map((column) => column.trim().split(' ')[0]);
+    const unquotedColumnNames = columnNames.filter(
+      (name) => !name.startsWith('"') && !name.endsWith('"'),
+    );
+    return unquotedColumnNames.length === 0;
   }
 }
-
