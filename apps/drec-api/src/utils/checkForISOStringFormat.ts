a1d2ff7541f941f79096654a06930279053ae26b
Aish1990 <akumaravadivu@sweyainfotech.com>
2024-05-20 15:26:23 +0530

    Fix: Code changes in checkforISOStringFormat

--- apps/drec-api/src/utils/checkForISOStringFormat.ts
+++ apps/drec-api/src/utils/checkForISOStringFormat.ts
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 export const isValidUTCDateFormat = (dateInput: string) => {
   const dateFormateToCheck = new RegExp(
-    /\d\d\d\d\-\d\d\-\d\dT\d\d:\d\d:\d\d\.\d{1,}Z/,
+    /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/
   );
   const dateFormat = 'YYYY-MM-DDTHH:mm:ssZ';
   const momentDateFormatToCheck = moment(dateInput).format(dateFormat);
