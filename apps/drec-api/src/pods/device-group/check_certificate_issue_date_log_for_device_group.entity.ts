39a2210b8eba99326873ec342e17a95be4b642f2
Aish1990 <akumaravadivu@sweyainfotech.com>
2024-07-19 20:46:30 +0530

    fixed CheckCertificateIssueDateLogForDeviceGroupEntity.ts

--- apps/drec-api/src/pods/device-group/check_certificate_issue_date_log_for_device_group.entity.ts
+++ apps/drec-api/src/pods/device-group/check_certificate_issue_date_log_for_device_group.entity.ts
@@ -2,12 +2,7 @@ import { ExtendedBaseEntity } from '@energyweb/origin-backend-utils';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import {
   IsString,
-  IsNotEmpty,
-  IsEnum,
-  IsBoolean,
   IsNumber,
-  IsOptional,
-  IsDate,
 } from 'class-validator';
 
 @Entity('check_certificate_issue_date_log_for_device_group')
