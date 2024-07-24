ea4d631e53f8bc9fafb885ffaf9667a1a1011d0a
Aish1990 <akumaravadivu@sweyainfotech.com>
2024-07-24 13:53:23 +0530

    fixed issuer.service.spec.ts.ts

--- apps/drec-api/src/pods/issuer/issuer.service.spec.ts
+++ apps/drec-api/src/pods/issuer/issuer.service.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationService } from '../organization/organization.service';
 import { IssuerService } from './issuer.service';
