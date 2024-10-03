b03555e5a18da65384e250f727a096fbc81ffd3e
Aish1990 <akumaravadivu@sweyainfotech.com>
2024-10-03 17:05:16 +0530

    yml file fixed

--- apps/drec-api/src/pods/reads/reads.service.ts
+++ apps/drec-api/src/pods/reads/reads.service.ts
@@ -76,7 +76,7 @@ export class ReadsService {
     private readonly organizationService: OrganizationService,
     private readonly eventBus: EventBus,
   ) {
-    const url = process.env.INFLUXDB_URL | 'http://localhost:8086';
+    const url = process.env.INFLUXDB_URL | "http://localhost:8086";
     const token = process.env.INFLUXDB_TOKEN;
     const org = process.env.INFLUXDB_ORG;
 
