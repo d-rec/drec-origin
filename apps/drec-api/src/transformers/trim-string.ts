f2fc0b85fa58bc51dd3b5480437b8c0e5ca25b96
Kingcedru <byishimocedrick@gmail.com>
2024-12-07 16:40:44 +0200

    fix: trim transformer

--- apps/drec-api/src/transformers/trim-string.ts
+++ apps/drec-api/src/transformers/trim-string.ts
@@ -2,4 +2,6 @@ import { applyDecorators } from '@nestjs/common';
 import { Transform } from 'class-transformer';
 
 export const Trim = (): PropertyDecorator =>
-  applyDecorators(Transform((value) => typeof value === 'string' ? value.trim() : value));
+  applyDecorators(
+    Transform((value) => (typeof value === 'string' ? value.trim() : value)),
+  );
