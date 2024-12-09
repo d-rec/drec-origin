33cee2b1c31ef1800d6ef05046db30f399ddc68d
Kingcedru <byishimocedrick@gmail.com>
2024-12-09 18:50:56 +0200

    fix: fixing capacity

--- apps/drec-api/src/index.ts
+++ apps/drec-api/src/index.ts
@@ -32,7 +32,7 @@ export async function startAPI(logger?: LoggerService): Promise<any> {
 
   const app = await NestFactory.create(DrecModule);
 
-  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false, transform: true }));
 
   app.enableShutdownHooks();
   app.enableCors();
