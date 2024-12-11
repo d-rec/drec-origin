733da48a106f2398ac164fea1afb93538ec0f01b
Kingcedru <byishimocedrick@gmail.com>
2024-12-11 12:13:42 +0200

    fix: prettier

--- apps/drec-api/src/sentry.ts
+++ apps/drec-api/src/sentry.ts
@@ -1,8 +1,7 @@
 import * as Sentry from '@sentry/nestjs';
 
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENV,
-    tracesSampleRate: 1.0,
-  });
-
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENV,
+  tracesSampleRate: 1.0,
+});
