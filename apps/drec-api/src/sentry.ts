ab66d4d0b653c922fd7f20a54a0cf0ea52d0c000
Yannick Musafiri <yannick@musafiri.co>
2024-12-11 17:47:16 +0200

    chore: add sentry enabled env variable

--- apps/drec-api/src/sentry.ts
+++ apps/drec-api/src/sentry.ts
@@ -1,10 +1,10 @@
 import * as Sentry from '@sentry/nestjs';
 
-const {SENTRY_DSN, SENTRY_ENV, MODE} = process.env;
+const {SENTRY_DSN, SENTRY_ENV, SENTRY_ENABLED} = process.env;
 
 Sentry.init({
   dsn: SENTRY_DSN,
   environment: SENTRY_ENV || 'development',
   tracesSampleRate: 1.0,
-  enabled: SENTRY_DSN && MODE && !(['dev', 'local', 'development'].includes(MODE)),
+  enabled: SENTRY_DSN && SENTRY_ENABLED?.toLowerCase() === 'true',
 });
