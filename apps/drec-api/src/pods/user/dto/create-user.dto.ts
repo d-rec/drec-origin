8a57c3b8a78631424adbd60d34b525ff5fdbd1bf
Divine Charlotte <divinemaina@gmail.com>
2024-12-11 15:51:10 +0200

    Move validations done in the user controllers into DTO (#459)
    * Mofeat:ve validations done in the user controllers into DTO
    
    * fix:fix validation
    
    * fix:fix validation
    
    * fix:fix validation
    
    * fix:remove unwanted file
    
    * fix:fix linters
    
    * fix:implement the requested changes
    
    * fix:implement requested changes

--- apps/drec-api/src/pods/user/dto/create-user.dto.ts
+++ apps/drec-api/src/pods/user/dto/create-user.dto.ts
@@ -1,6 +1,5 @@
-import { ApiProperty, PickType, IntersectionType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { UserDTO } from './user.dto';
-import { OrganizationDTO } from '../../organization/dto/organization.dto';
 import {
   IsNotEmpty,
   IsString,
@@ -8,9 +7,12 @@ import {
   MaxLength,
   IsOptional,
   IsUUID,
+  IsIn,
 } from 'class-validator';
 import { UserORGRegistrationData } from '../../../models';
 import { Match } from '../decorators/match.decorator';
+import { Trim } from '../../../transformers/string';
+import { Role } from '../../../utils/enums/role.enum';
 // export class CreateUserDTO
 //   extends PickType(UserDTO, [
 //     'title',
@@ -36,10 +38,7 @@ import { Match } from '../decorators/match.decorator';
 // }
 
 export class CreateUserORGDTO
-  extends IntersectionType(
-    PickType(UserDTO, ['firstName', 'lastName', 'email'] as const),
-    PickType(OrganizationDTO, ['organizationType'] as const),
-  )
+  extends PickType(UserDTO, ['firstName', 'lastName', 'email'] as const)
   implements UserORGRegistrationData
 {
   @ApiProperty({ type: String })
@@ -58,6 +57,7 @@ export class CreateUserORGDTO
 
   @ApiProperty({ type: String })
   @IsString()
+  @Trim()
   @IsNotEmpty()
   orgName?: string;
 
@@ -91,4 +91,12 @@ export class CreateUserORGDTO
 
   @IsOptional()
   orgid?: number;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  @IsIn([Role.Developer, Role.ApiUser, Role.Buyer], {
+    message: 'organizationType value should be Developer/Buyer/ApiUser',
+  })
+  organizationType: string;
 }
