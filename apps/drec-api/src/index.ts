aa12322f85a1f6546520e68ee6763ad0add5ebd1
Kingcedru <byishimocedrick@gmail.com>
2024-12-11 12:08:36 +0200

    fix: sentry environments

--- apps/drec-api/src/index.ts
+++ apps/drec-api/src/index.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import fs from 'fs';
 import { DrecModule } from './drec.module';
-import './instrument';
+import './sentry';
 import * as PortUtils from './port';
 
 export { DrecModule };
--- apps/drec-api/src/sentry.ts
+++ apps/drec-api/src/sentry.ts
@@ -1,9 +1,8 @@
 import * as Sentry from '@sentry/nestjs';
 
-if (process.env.NODE_ENV !== 'local') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    environment: process.env.NODE_ENV,
+    environment: process.env.SENTRY_ENV,
     tracesSampleRate: 1.0,
   });
-}
+
