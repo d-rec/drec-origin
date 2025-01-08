e163357b4950e96e73138d87d5796ffafa48dd7c
Yannick Musafiri <yannick@musafiri.co>
2025-01-08 14:32:43 +0200

    fix: organizationId handling

--- apps/drec-api/src/pods/reads/reads.controller.ts
+++ apps/drec-api/src/pods/reads/reads.controller.ts
@@ -344,6 +344,7 @@ export class ReadsController extends BaseReadsController {
         user,
         organizationId: measurements.organizationId,
       });
+      user.organizationId = measurements.organizationId;
     }
 
     if (id.trim() === '' && id.trim() === undefined) {
