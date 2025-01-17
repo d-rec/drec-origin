481c244f3ffd7174d70ac72ff1841fadbf2b5e4e
Yannick Musafiri <yannick@musafiri.co>
2025-01-17 19:38:52 +0200

    chore: add late ongoing debugging logs

--- apps/drec-api/src/pods/issuer/issuer.service.ts
+++ apps/drec-api/src/pods/issuer/issuer.service.ts
@@ -1092,12 +1092,14 @@ export class IssuerService {
 
   private async triggerOngoingLateIssuance(): Promise<void> {
     this.logger.debug('late ongoing issuance');
-    this.logger.debug('Called every 2hr to check for issuance of certificates');
+    this.logger.debug('Called every 8hr to check for issuance of certificates');
     const lateOngoing = await this.deviceService.findAllLateCycle();
     if (lateOngoing) {
       for (const element of lateOngoing) {
         const group = await this.groupService.findOne({ id: element.groupId });
-        this.logger.debug('Processing late ongoing issuance for', element.id);
+        this.logger.debug(
+          'Processing late ongoing issuance for::', element.device_externalid,
+        );
         if (!group) {
           this.logger.error('LateOngoing group is missing');
           continue; // Skip to the next element if the group is missing
@@ -1192,7 +1194,7 @@ export class IssuerService {
                 new Date(element.late_end_date).toISOString(),
               );
             }
-
+            this.logger.debug('Late ongoing Issue Certificate For::', element.device_externalid);
             await this.lateOngoingIssueCertificateForGroup(
               newGroupWithSingleDevice,
               startDate,
@@ -1219,6 +1221,12 @@ export class IssuerService {
               newGroupWithSingleDevice.devices[0].externalId,
               readsFilter,
             );
+          this.logger.debug(
+            'Device Reads For:: ' + element.device_externalid,
+            'From: ' + startDate.toString(),
+            'To: ' + endDate.toString(),
+            'Equal to ' + allReadsForDeviceBetweenTimeRange?.length,
+          );
           if (allReadsForDeviceBetweenTimeRange.length > 0) {
             this.logger.verbose('if read are available in date range');
             await this.deviceService.updatelateongoing(
