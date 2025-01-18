9e70a2f007107fb4c2b298ebe11aa32b8e02d86d
Kingcedru <byishimocedrick@gmail.com>
2025-01-18 21:12:41 +0200

    fix: bulk-upload tests

---
+++ apps/drec-api/src/pods/bulk-upload/bulk-upload.service.spec.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { OrganizationService } from '../organization/organization.service';
+import { EventBus } from '@nestjs/cqrs';
+import { FileService } from '../file/file.service';
+import { BulkUploadFailedLogEntity } from '../bulk-upload/bulk-uploads-failed-logs.entity';
+import { BulkUploadEntity } from '../bulk-upload/bulk-uploads.entity';
+import { BulkUploadService } from './bulk-upload.service';
+import { ReadsService } from '../reads/reads.service';
+
+jest.mock('@influxdata/influxdb-client', () => {
+  return {
+    InfluxDB: jest.fn().mockImplementation(() => {
+      return {
+        getQueryApi: jest.fn().mockReturnValue({
+          queryRows: jest.fn(),
+        }),
+      };
+    }),
+  };
+});
+
+describe('BulkUploadService', () => {
+  let service: BulkUploadService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BulkUploadService,
+        {
+          provide: getRepositoryToken(BulkUploadFailedLogEntity),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(BulkUploadEntity),
+          useValue: {},
+        },
+        {
+          provide: ReadsService,
+          useValue: {},
+        },
+        {
+          provide: FileService,
+          useValue: {
+            uploadFile: jest.fn().mockResolvedValue('mock-file-url'),
+            retrieveFile: jest.fn().mockResolvedValue('mock-file-content'),
+          },
+        },
+        {
+          provide: OrganizationService,
+          useValue: {} as any,
+        },
+        {
+          provide: EventBus,
+          useValue: {
+            publish: jest.fn(),
+            subscribe: jest.fn(),
+            unsubscribe: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<BulkUploadService>(BulkUploadService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
