e10f412196b105b64a477a0c05a8f24da49d5c84
Kingcedru <byishimocedrick@gmail.com>
2025-02-06 11:31:22 +0200

    feat: improve withAuth Guard file

--- apps/drec-api/src/guards/WithoutAuthGuard.ts
+++ apps/drec-api/src/guards/WithoutAuthGuard.ts
@@ -12,6 +12,8 @@ import { Role } from '../utils/enums';
 import { EmailConfirmationService } from '../pods/email-confirmation/email-confirmation.service';
 import { OauthClientCredentialsService } from '../pods/user/oauth_client.service';
 import { isEmail } from 'class-validator';
+import { UrlPath } from '../utils/enums/url-path.enum';
+
 @Injectable()
 export class WithoutAuthGuard implements CanActivate {
   private readonly logger = new Logger(WithoutAuthGuard.name);
@@ -26,79 +28,83 @@ export class WithoutAuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    this.logger.verbose(`With in canActivate`);
+    this.logger.verbose(`Within canActivate`);
 
     const request = context.switchToHttp().getRequest();
+    const pathSegment = request.url.split('/')[3];
     let user: IUser;
 
-    if (request.url.split('/')[3] === 'forget-password') {
-      user = await this.userService.findByEmail(request.body.email);
-    } else if (
-      request.url.split('/')[3] === 'confirm-email' ||
-      request.url.split('/')[3] === 'reset'
-    ) {
-      if (isEmail(request.params.token)) {
-        console.log('Token Emails', request.params.token);
-        user = await this.userService.findByEmail(request.params.token);
-      } else {
-        user = (
-          await this.emailConfirmationService.findOne({
-            token: request.params.token,
-          })
-        ).user;
+    switch (pathSegment) {
+      case UrlPath.ForgetPassword:
+        user = await this.userService.findByEmail(request.body.email);
+        break;
+
+      case UrlPath.ConfirmEmail:
+      case UrlPath.ResetPassword:
+        if (isEmail(request.params.token)) {
+          this.logger.verbose(`Token is an email: ${request.params.token}`);
+          user = await this.userService.findByEmail(request.params.token);
+        } else {
+          user = (
+            await this.emailConfirmationService.findOne({
+              token: request.params.token,
+            })
+          ).user;
+        }
+        break;
+
+      case UrlPath.Register: {
+        const userData = await this.userService.findOne({ role: Role.Admin });
+        const userRoles = [Role.Developer, Role.ApiUser];
+
+        if (!request.body.api_user_id) {
+          if (userRoles.includes(request.body.organizationType)) {
+            user = userData;
+          }
+        } else if (request.body.api_user_id !== userData.api_user_id) {
+          if (userRoles.includes(request.body.organizationType)) {
+            user = await this.userService.findOne({
+              role: Role.ApiUser,
+              api_user_id: request.body.api_user_id,
+            });
+
+            if (!user) {
+              throw new UnauthorizedException({
+                statusCode: 401,
+                message: 'Requested api user is not available',
+              });
+            }
+          }
+        } else if (request.body.organizationType === Role.ApiUser) {
+          const apiUser =
+            await this.oauthClientCredentialsService.createAPIUser();
+          request.body.api_user_id = apiUser.api_user_id;
+        }
+        break;
       }
-    } else if (request.url.split('/')[3] === 'register') {
-      const userData = await this.userService.findOne({ role: Role.Admin });
-      if (
-        request.body.api_user_id == undefined &&
-        (request.body.organizationType === 'Developer' ||
-          request.body.organizationType === Role.Buyer)
-      ) {
-        user = userData;
-      } else if (
-        request.body.api_user_id != undefined &&
-        request.body.api_user_id != userData.api_user_id &&
-        (request.body.organizationType === 'Developer' ||
-          request.body.organizationType === Role.Buyer)
-      ) {
+
+      case UrlPath.ExportAccesskey:
         user = await this.userService.findOne({
           role: Role.ApiUser,
-          api_user_id: request.body.api_user_id,
+          api_user_id: request.params.api_user_id,
         });
-        if (!user) {
-          throw new UnauthorizedException({
-            statusCode: 401,
-            message: 'Requested apiuser is not available',
-          });
-        }
-      } else if (request.body.organizationType === Role.ApiUser) {
-        const apiUser =
-          await this.oauthClientCredentialsService.createAPIUser();
-        request.body.api_user_id = apiUser.api_user_id;
-      }
-    } else if (request.url.split('/')[3] === 'export-accesskey') {
-      user = await this.userService.findOne({
-        role: Role.ApiUser,
-        api_user_id: request.params.api_user_id,
-      });
-    } else if (request.url.split('/')[3] === 'login') {
-      user = await this.userService.findByEmail(request.body.username);
+        break;
+
+      case UrlPath.Login:
+        user = await this.userService.findByEmail(request.body.username);
+        break;
+
+      default:
+        break;
     }
 
-    if (
-      request.body.organizationType === undefined &&
-      user.role != Role.Admin &&
-      user.role != Role.ApiUser &&
-      user.api_user_id !=
-        ((await this.userService.findOne({ role: Role.Admin })) as IUser)
-          .api_user_id
-    ) {
+    if (!user) {
       throw new UnauthorizedException({
         statusCode: 401,
         message: 'Unauthorized',
       });
-      //return false;
     }
+
     request.user = user;
     return true;
   }
---
+++ apps/drec-api/src/utils/enums/url-path.enum.ts
@@ -0,0 +1,8 @@
+export enum UrlPath {
+  ResetPassword = 'reset',
+  ConfirmEmail = 'confirm-email',
+  ForgetPassword = 'forget-password',
+  Register = 'register',
+  Login = 'login',
+  ExportAccesskey = 'export-accesskey',
+}
