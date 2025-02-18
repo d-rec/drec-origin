de8b9f265b586b10939a93bea55a231c06493c0c
Cedrick  Byishimo <cedrick@drec.energy>
2025-02-18 14:28:15 +0200

    fix: reset password (#493)
    * fix: reset password
    
    * feat: changes IsEmail decorator
    
    * feat: improve withAuth Guard file
    
    * refactor: implement the switch case
    
    * refactor: remove console logs
    
    * refactor: improve variable name
    
    * feat: add request body values check

---
+++ apps/drec-api/src/utils/enums/url-path.enum.ts
@@ -0,0 +1,8 @@
+export enum UrlPath {
+  ResetPassword = 'reset',
+  ConfirmEmail = 'confirm-email',
+  ForgetPassword = 'forget-password',
+  Register = 'register',
+  Login = 'login',
+  ExportAccessKey = 'export-accesskey',
+}
