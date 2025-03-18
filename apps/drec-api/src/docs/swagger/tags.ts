b8bb54ab9e4e9f856637234ce179f54a85aa1200
Charlotte Divine <divinemaina@gmail.com>
2025-03-18 10:11:37 +0200

    chore: improve organization endpoints documentation

--- apps/drec-api/src/docs/swagger/tags.ts
+++ apps/drec-api/src/docs/swagger/tags.ts
@@ -9,6 +9,11 @@ const tags: { name: string; description: string }[] = [
     description:
       'All endpoints related to user management. This includes functionalities such as retrieving user profiles, updating user information, managing user passwords, confirming email addresses, and handling user registrations. Endpoints under this tag facilitate the management of user data and interactions, ensuring that user-related operations are organized and easily accessible for developers.',
   },
+  {
+    name: 'Organization',
+    description:
+      'All endpoints related to organization management. This includes functionalities such as retrieving organization details for the authenticated user, managing all organizations associated with an API user, fetching users within an organization, handling user role changes, managing invitations, and setting blockchain addresses. These endpoints ensure comprehensive management of organizational data and user interactions within the organization, providing a structured approach to organization-related operations.',
+  },
 ];
 
 export default tags;
