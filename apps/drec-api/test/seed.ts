a8274d4c727a7dd78e45a5800b2447a177020442
Cedrick  Byishimo <cedrick@drec.energy>
2025-03-28 00:46:10 -0700

    feat: create organization type enum (#544)
    * feat: create organization type enum
    
    * feat: apply organizationTypes
    
    * fix: lint errors
    
    * feat: add organization type on organization
    
    * feat: add admin to organization type
    
    * add organizatio to tests
    
    * refactor: rename userRole to user organization types
    
    * feat: add organization type to invitation test

--- apps/drec-api/test/seed.ts
+++ apps/drec-api/test/seed.ts
@@ -10,6 +10,7 @@ import {
   Installation,
   OffTaker,
   OrganizationStatus,
+  OrganizationType,
   Sector,
   StandardCompliance,
   UserStatus,
@@ -27,7 +28,7 @@ export const testOrgs: OrganizationDTO[] = [
     country: 'DE',
     status: OrganizationStatus.Active,
     blockchainAccountAddress: 'blockchainAccountAddress111',
-    organizationType: 'Developer',
+    organizationType: OrganizationType.Developer,
   },
   {
     id: 11,
@@ -38,7 +39,7 @@ export const testOrgs: OrganizationDTO[] = [
     country: 'null',
     status: OrganizationStatus.Active,
     blockchainAccountAddress: 'blockchainAccountAddress222',
-    organizationType: 'Buyer',
+    organizationType: OrganizationType.Buyer,
   },
   {
     id: 12,
@@ -49,7 +50,7 @@ export const testOrgs: OrganizationDTO[] = [
     country: 'DE',
     status: OrganizationStatus.Active,
     blockchainAccountAddress: 'blockchainAccountAddress333',
-    organizationType: 'Admin',
+    organizationType: OrganizationType.Admin,
   },
   {
     id: 13,
@@ -60,7 +61,7 @@ export const testOrgs: OrganizationDTO[] = [
     country: 'DE',
     status: OrganizationStatus.Active,
     blockchainAccountAddress: 'blockchainAccountAddress444',
-    organizationType: 'Developer',
+    organizationType: OrganizationType.Developer,
   },
 ];
 
@@ -70,28 +71,28 @@ export const testUsers: Omit<CreateUserOrgDTO, 'organizationId'>[] = [
     lastName: 'Williams',
     email: 'owner2@mailinator.com',
     password: '******123',
-    organizationType: 'Developer',
+    organizationType: OrganizationType.Developer,
   },
   {
     firstName: 'John',
     lastName: 'Buyer',
     email: 'buyer2@mailinator.com',
     password: '******123',
-    organizationType: 'Buyer',
+    organizationType: OrganizationType.Buyer,
   },
   {
     firstName: 'Joe',
     lastName: 'Miller2',
     email: 'admin2@mailinator.com',
     password: '******123',
-    organizationType: 'ApiUser',
+    organizationType: OrganizationType.ApiUser,
   },
   {
     firstName: 'Maria',
     lastName: 'Williams',
     email: 'owner3@mailinator.com',
     password: '******123',
-    organizationType: 'Developer',
+    organizationType: OrganizationType.Developer,
   },
 ];
 
