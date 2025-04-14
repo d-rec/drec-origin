dd7372ad5350e6d8ff448340fe2109fa9319f046
Kingcedru <byishimocedrick@gmail.com>
2025-04-14 14:12:49 +0200

    fix:document uplaod test

--- apps/drec-api/src/pods/document-uploads/document-uploads.service.spec.ts
+++ apps/drec-api/src/pods/document-uploads/document-uploads.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DocumentUploadsService } from './document-uploads.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DocumentEntity } from './entities/documents.entity';
+import { FileService } from '../file/file.service';
 
 describe('DocumentUploadsService', () => {
   let service: DocumentUploadsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DocumentUploadsService],
+      providers: [
+        DocumentUploadsService,
+        {
+          provide: getRepositoryToken(DocumentEntity),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: FileService,
+          useValue: {
+            upload: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<DocumentUploadsService>(DocumentUploadsService);
