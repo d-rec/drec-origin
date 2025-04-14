4365fedeb37b8238f9458ffb1c0835d15d20087f
Kingcedru <byishimocedrick@gmail.com>
2025-04-14 14:05:04 +0200

    fix: unit test

--- apps/drec-api/src/pods/document-uploads/document-uploads.controller.spec.ts
+++ apps/drec-api/src/pods/document-uploads/document-uploads.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DocumentUploadsController } from './document-uploads.controller';
 import { DocumentUploadsService } from './document-uploads.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DocumentEntity } from './entities/documents.entity';
+import { FileService } from '../file/file.service';
 
 describe('DocumentUploadsController', () => {
   let controller: DocumentUploadsController;
@@ -8,7 +11,22 @@ describe('DocumentUploadsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DocumentUploadsController],
-      providers: [DocumentUploadsService],
+      providers: [
+        DocumentUploadsService,
+        {
+          provide: getRepositoryToken(DocumentEntity),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: FileService,
+          useValue: {
+            upload: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<DocumentUploadsController>(
