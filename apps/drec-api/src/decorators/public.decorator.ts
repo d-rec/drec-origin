067b55f0fc6e47aacdf4b51d9a881cee60838521
Kingcedru <byishimocedrick@gmail.com>
2025-04-16 18:46:19 +0200

    fix: lint and prrettiers

--- apps/drec-api/src/decorators/public.decorator.ts
+++ apps/drec-api/src/decorators/public.decorator.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 export const IS_PUBLIC_KEY = 'isPublic';
 
 export const Public = () => {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   return (target: any, key?: string | symbol, descriptor?: any) => {
     Reflect.defineMetadata(IS_PUBLIC_KEY, true, descriptor.value);
     return descriptor;
