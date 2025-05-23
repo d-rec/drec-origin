44ffc0c0e77a2188323740bbfe477c22aa71c6f8
Kingcedru <byishimocedrick@gmail.com>
2025-05-23 11:49:56 +0300

    refactor: add the NODE_ENV varible in the env

--- apps/drec-api/src/pods/email-confirmation/email-confirmation.service.ts
+++ apps/drec-api/src/pods/email-confirmation/email-confirmation.service.ts
@@ -279,7 +279,7 @@ export class EmailConfirmationService {
     const MOCK_EMAIL_TOKEN = '123456';
     return {
       token:
-        process.env.MODE === 'test'
+        process.env.NODE_ENV === 'test'
           ? MOCK_EMAIL_TOKEN
           : crypto.randomBytes(64).toString('hex'),
       expiryTimestamp: Math.floor(
