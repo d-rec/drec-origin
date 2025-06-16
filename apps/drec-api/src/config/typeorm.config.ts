b7b378f558fb2f2d27139c426d302bef948c28c6
Yannick Musafiri <yannick@musafiri.co>
2025-06-16 13:34:54 +0300

    hotfix: add missing db host in config

--- apps/drec-api/src/config/typeorm.config.ts
+++ apps/drec-api/src/config/typeorm.config.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const getDBConfig = () => ({
+  host: process.env.DB_HOST ?? 'localhost',
   port: Number(process.env.DB_PORT) ?? 5432,
   username: process.env.DB_USERNAME ?? 'postgres',
   password: process.env.DB_PASSWORD ?? 'postgres',
