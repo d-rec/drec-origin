9fd8a071058a6f04da7f71d6cb7809c120650d53
Agnes Iradukunda <101483232+kunda4@users.noreply.github.com>
2025-06-16 09:53:33 +0200

    feat: add database replication routing connection (#628)
    * feat: configure the use of main and replica db
    
    * chore: fix linting issues
    
    ---------
    
    Co-authored-by: Yannick Musafiri <yannick@musafiri.co>
    Co-authored-by: Yannick Musafiri <yannick@drec.energy>

---
+++ apps/drec-api/src/config/typeorm.config.ts
@@ -0,0 +1,44 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const getDBConfig = () => ({
+  port: Number(process.env.DB_PORT) ?? 5432,
+  username: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_DATABASE ?? 'origin',
+});
+
+const getMainDBConfig = () => {
+  if (process.env.DATABASE_URL) {
+    return {
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
+  }
+
+  return getDBConfig();
+};
+
+const getReplicaDBConfig = () => {
+  if (!process.env.DB_REPLICA_HOST) return [];
+  return [
+    {
+      ...getDBConfig(),
+      host: process.env.DB_REPLICA_HOST,
+      port:
+        Number(process.env.DB_REPLICA_PORT) ??
+        Number(process.env.DB_PORT) ??
+        5432,
+    },
+  ];
+};
+
+export const getTypeOrmConfig = (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  replication: {
+    master: getMainDBConfig(),
+    slaves: getReplicaDBConfig(),
+  },
+  logging: ['info'],
+});
