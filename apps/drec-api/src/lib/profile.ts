a335bdf2aaf606134174cb43166d7b6b7be57932
Yannick Musafiri <yannick@musafiri.co>
2025-06-24 19:57:16 +0200

    hotfix: handle async and non async function in the profiler

--- apps/drec-api/src/lib/profile.ts
+++ apps/drec-api/src/lib/profile.ts
@@ -1,21 +1,33 @@
 import { performance } from 'perf_hooks';
 import { Logger } from '@nestjs/common';
 
+
 function Profile(): MethodDecorator {
   return function(target, propertyKey, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const logger = new Logger(target.constructor.name);
-    descriptor.value = async function(...args: any[]) {
-      const name = `${String(propertyKey)}`;
-      const start = performance.now();
+    const isAsync = originalMethod.constructor.name === 'AsyncFunction';
 
-      const result = await originalMethod.apply(this, args);
-
+    const logPerformance = (name: string, start: number) => {
       const end = performance.now();
       const duration = (end - start).toFixed(2);
       logger.warn(`${name}: Executed in ${duration} ms`);
+    };
 
-      return result;
+    descriptor.value = function(...args: any[]) {
+      const name = `${String(propertyKey)}`;
+      const start = performance.now();
+
+      if(!isAsync){
+        const result = originalMethod.apply(this, args);
+        logPerformance(name, start);
+        return result;
+      }
+
+      return originalMethod.apply(this, args).then((result: any) => {
+        logPerformance(name, start);
+        return result;
+      });
     };
 
     return descriptor;
