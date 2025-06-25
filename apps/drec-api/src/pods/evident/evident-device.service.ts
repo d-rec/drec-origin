3280ccdc1b24e7c9501586f76f4e7c51d92cdf1b
Yannick Musafiri <yannick@drec.energy>
2025-06-25 15:19:59 +0200

    feat: convert capacity to MWh for a device before registering it on evident (#650)
    * feat: convert capacity to MWh for a device before registering it on evident
    
    * fix: circular dependency
    
    ---------
    
    Co-authored-by: Kingcedru <byishimocedrick@gmail.com>

--- apps/drec-api/src/pods/evident/evident-device.service.ts
+++ apps/drec-api/src/pods/evident/evident-device.service.ts
@@ -7,13 +7,14 @@ import { Queue } from 'bull';
 import { getCountryCodeAlpha2 } from '../../utils/get-country-code-alpha-2';
 import { DocumentType } from '../document-uploads/entities/documents.entity';
 import FormData from 'form-data';
-import { convertToWh } from '../../utils/convert-to-power-units';
+import { convertToPowerUnit } from '../../utils/convert-to-power-units';
 import {
   EvidentDeviceDetailsPayload,
   EvidentRegistrationStatus,
 } from '../../types/evident';
 import getFileData from '../../lib/helpers/getFileData';
 import { EvidentService } from './evident.service';
+import { Unit } from '@energyweb/energy-api-influxdb';
 
 @Injectable()
 export class EvidentDeviceService {
@@ -217,7 +218,11 @@ export class EvidentDeviceService {
     files: string[],
   ): any {
     const alpha2CountryCode = getCountryCodeAlpha2(device.countryCode);
-    const convertCapacityToMwh = convertToWh(device.capacity, 'kWh');
+    const convertCapacityToMwh = convertToPowerUnit({
+      value: device.capacity,
+      unit: Unit.kWh,
+      targetUnit: Unit.MWh,
+    });
     return {
       deviceType: `/device_types/${device.deviceTypeCode}`,
       fuel: `/fuels/${device.fuelCode}`,
