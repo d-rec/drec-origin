ce1e1d02e10b1f0b2a112ab7706b9d1f53926366
Yannick Musafiri <yannick@musafiri.co>
2025-07-07 15:53:54 +0200

    chore: fix failing tests

--- apps/drec-api/test/setup-e2e.ts
+++
@@ -1,9 +0,0 @@
-jest.mock(
-    '../src/pods/evident/mail/evident-draft-device-registration.template',
-    () => ({
-      __esModule: true,
-      default: () => null,
-      getEvidentDraftDeviceRegistrationSubject: () => 'Mock subject',
-    })
-  );
-  
