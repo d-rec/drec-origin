147bd508ee9d95fcb0bbbf53733bc1045e21f7ba
Divine Charlotte <divinemaina@gmail.com>
2025-07-09 11:32:28 +0200

    Ft/evident draft device submission notification (#657)
    * feat: send notification when a device is created as a draft on Evident
    
    * fix: fix the error being thrown by mail service
    
    * fix:separate email template from the service
    
    * feat: add support for react email templates
    
    * fix:update rush
    
    * fix: mock files
    
    * fix: build emails
    
    * fix: improve the tsconfig location file
    
    * fix: improve the tsconfig to extend the tsconfing globally
    
    * fix: accept all files extensions
    
    * chore: fix failing tests
    
    * fix: remove unused variables
    
    * fix: apiuser generation
    
    * feat: send email to market intermediary
    
    * chore: remove console.logs
    
    ---------
    
    Co-authored-by: Yannick Musafiri <yannick@musafiri.co>
    Co-authored-by: Kingcedru <byishimocedrick@gmail.com>

--- apps/drec-api/src/pods/certificate-log/dto/certificatewithdevicelog.dto.ts
+++ apps/drec-api/src/pods/certificate-log/dto/certificatewithdevicelog.dto.ts
@@ -2,6 +2,8 @@ import { Certificate } from '@energyweb/issuer-api';
 import { CheckCertificateIssueDateLogForDeviceEntity } from '../../device/check_certificate_issue_date_log_for_device.entity';
 import { ICertificateReadModel } from '@energyweb/origin-247-certificate';
 import { ICertificateMetadata } from '../../../utils/types';
+import { IClaim } from '@energyweb/issuer';
+import { CertificateTransaction } from '@energyweb/origin-247-certificate/dist/js/src/offchain-certificate/repositories/CertificateReadModel/CertificateReadModel.entity';
 
 export class CertificateWithPerDeviceLog extends Certificate {
   id: number;
@@ -15,11 +17,22 @@ export class CertificateWithPerDeviceLog extends Certificate {
   metadata: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface CertificateNewWithPerDeviceLog
-  extends ICertificateReadModel<ICertificateMetadata> {}
-
-export class CertificateNewWithPerDeviceLog {
+export class CertificateNewWithPerDeviceLog
+  implements ICertificateReadModel<ICertificateMetadata>
+{
+  internalCertificateId: number;
+  blockchainCertificateId: number;
+  deviceId: string;
+  generationStartTime: number;
+  generationEndTime: number;
+  creationTime: number;
+  metadata: ICertificateMetadata;
+  creationBlockHash: string;
+  claims: IClaim[];
+  owners: Record<string, string>;
+  claimers: Record<string, string>;
+  isSynced: boolean;
+  transactions: CertificateTransaction[];
   certificateStartDate: string;
   certificateEndDate: string;
   perDeviceCertificateLog: CheckCertificateIssueDateLogForDeviceEntity[];
