dc0be7414e22d8fb47c8aeadb3dac8ea787ef619
Kingcedru <byishimocedrick@gmail.com>
2025-07-24 15:33:59 +0300

    feat: Add a button to login on email templates

--- apps/drec-api/src/mail/layouts/default.layout.tsx
+++ apps/drec-api/src/mail/layouts/default.layout.tsx
@@ -5,17 +5,20 @@ import {
   Html,
   Section,
   Text,
+  Button,
 } from '@react-email/components';
 import * as React from 'react';
 
 type DefaultMailLayoutProps = {
   language?: string | undefined;
   children: React.ReactNode;
+  logInURL?: string;
 };
 
 export default function DefaultMailLayout({
   language = 'en',
   children,
+  logInURL = process.env.UI_BASE_URL,
 }: DefaultMailLayoutProps): React.JSX.Element {
   return (
     <Html lang={language}>
@@ -26,6 +29,9 @@ export default function DefaultMailLayout({
         <Section>
           <Container>
             {children}
+            <Button style={{ backgroundColor: '#2557d3', color: '#fff', padding: '10px 20px', borderRadius: '5px' }} href={logInURL}>
+              Log In to Your Account
+            </Button>
             <Text>
               Best regards, <br />
               D-REC Team
