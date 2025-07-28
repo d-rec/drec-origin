a2d9cb4b41be3911a5cc5a1d807dfcb9758ea45c
Cedrick  Byishimo <cedrick@drec.energy>
2025-07-28 13:52:47 +0200

    Feat: auto assign device issuer (#669)
    * feat: auto assign issuer
    
    * feat: improve the get country method
    
    * refactor: remove logs
    
    * feat: improve checking for response length
    
    * fix: prettier
    
    * feat: create a function for getting issuer
    
    * fix: prettier and adding else condition
    
    * feat: improved the capacity condition

--- apps/drec-api/src/utils/get-country-code-alpha-2.ts
+++
@@ -1,8 +0,0 @@
-import { countryCodesList } from '../models/country-code';
-
-export function getCountryCodeAlpha2(countryCode: string): string {
-  const country = countryCodesList.find(
-    (country) => country.alpha3 === countryCode,
-  );
-  return country ? country.alpha2 : countryCode;
-}
