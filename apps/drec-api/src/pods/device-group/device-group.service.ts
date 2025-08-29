0547445f83223b3ad087bc6f872b0f179258521e
Divine <divinemaina@gmail.com>
2025-08-29 17:04:26 +0200

    chore: filtering device groups device serialNumber

--- apps/drec-api/src/pods/device-group/device-group.service.ts
+++ apps/drec-api/src/pods/device-group/device-group.service.ts
@@ -342,6 +342,27 @@ export class DeviceGroupService {
           });
         }
       }
+      if (filterDTO.deviceIds) {
+        const deviceIdsArray = filterDTO.deviceIds
+          .toString()
+          .split(',')
+          .map((id) => Number(id));
+        query.andWhere(
+          new Brackets((qb) => {
+            deviceIdsArray?.forEach((deviceId, index) => {
+              if (index === 0) {
+                qb.where(`device.id = :deviceId${index}`, {
+                  [`deviceId${index}`]: deviceId,
+                });
+              } else {
+                qb.orWhere(`device.id = :deviceId${index}`, {
+                  [`deviceId${index}`]: deviceId,
+                });
+              }
+            });
+          }),
+        );
+      }
     }
 
     const [groups, totalCount] = await query
@@ -350,17 +371,19 @@ export class DeviceGroupService {
       .getManyAndCount();
     const totalPages = Math.ceil(totalCount / limit);
     const groupsWithOrganization = await Promise.all(
-      groups.map(async (group: DeviceGroupDTO) => {
+      groups.map(async (group: DeviceGroup) => {
         const organization = await this.organizationService.findOne(
           group.organizationId,
         );
         group.organization = {
           name: organization.name,
         };
+        const devices = await this.deviceService.findByIds(group.deviceIdsInt);
+        group.devices = devices;
+
         return group;
       }),
     );
-
     return {
       groupedData: groupsWithOrganization,
       currentPage: pageNumber,
