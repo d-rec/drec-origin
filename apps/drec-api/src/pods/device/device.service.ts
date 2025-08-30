6fc9269c31b4e559f3fe620661d12761d2efaf79
Yannick Musafiri <yannick@musafiri.co>
2025-08-30 17:28:53 +0300

    fix: sync devices only registered on evident

--- apps/drec-api/src/pods/device/device.service.ts
+++ apps/drec-api/src/pods/device/device.service.ts
@@ -25,6 +25,7 @@ import {
   FindOneOptions,
   FindOperator,
   In,
+  IsNull,
   LessThan,
   LessThanOrEqual,
   MoreThanOrEqual,
@@ -559,7 +560,11 @@ export class DeviceService {
   }
 
   async syncStatusesWithEvident(): Promise<void> {
-    const devices = await this.repository.find();
+    const devices = await this.repository.find({
+      where: {
+        evidentDeviceId: Not(IsNull()),
+      }
+    });
     for (const device of devices) {
       try {
         const updatedStatus = await this.evidentDeviceService.getStatus(
