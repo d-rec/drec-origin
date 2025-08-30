35164e78c8f1cfc4f9a4184ff4113f2c5104f771
Yannick Musafiri <yannick@musafiri.co>
2025-08-30 18:00:02 +0300

    chore(release): 0.7.1

--- apps/drec-api/src/lib/redis.ts
+++
@@ -1,8 +0,0 @@
-import Redis from 'ioredis';
-
-export const getRedisClient = (): Redis => {
-  return new Redis({
-    host: process.env.REDIS_URL ?? 'localhost',
-    port: Number(process.env.REDIS_PORT ?? 6379),
-  });
-};
