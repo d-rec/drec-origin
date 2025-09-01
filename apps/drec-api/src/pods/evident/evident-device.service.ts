7b7791dbede761ac650c60be85b44a0a063eb0c5
Kingcedru <byishimocedrick@gmail.com>
2025-09-01 15:44:43 +0300

    refactor: add the error logger at the top

--- apps/drec-api/src/pods/evident/evident-device.service.ts
+++ apps/drec-api/src/pods/evident/evident-device.service.ts
@@ -72,9 +72,10 @@ export class EvidentDeviceService {
       device.evidentDeviceId = response.data.code;
       return await this.saveDeviceDetails(device, files);
     } catch (error) {
+      this.logger.error('Error registering device:', error.message);
       if (error.isAxiosError && error?.response) {
         const { response } = error;
-        this.logger.error('Error registering Evident device:', {
+        this.logger.error({
           axiosError: {
             status: response?.status,
             statusText: response?.statusText,
