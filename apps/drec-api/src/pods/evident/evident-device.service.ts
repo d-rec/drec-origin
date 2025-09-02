53c14d90342894b87d4b2cfc85e5e8751542cb8b
Cedrick  Byishimo <cedrick@drec.energy>
2025-09-02 09:10:01 +0200

    Ft/auto issuer from db (#733)
    * feat: auto assign issuer from the database
    
    * refactor: restore file service
    
    * refactor: remove type
    
    * fix: lint errors
    
    ---------
    
    Co-authored-by: Yannick Musafiri <yannick@drec.energy>

--- apps/drec-api/src/pods/evident/evident-device.service.ts
+++ apps/drec-api/src/pods/evident/evident-device.service.ts
@@ -115,7 +115,8 @@ export class EvidentDeviceService {
     );
 
     if (device.capacity >= SMALL_DEVICES_MAX_CAPACITY) {
-      payload.issuer = await this.getIssuerForDevice(device);
+      const issuerId = await this.getIssuerForDevice(device);
+      payload.issuer = `/organisations/${issuerId}`;
     } else {
       payload.issuer = `/organisations/${this.issuerId}`;
     }
@@ -270,17 +271,15 @@ export class EvidentDeviceService {
   }
 
   private async getIssuerForDevice(device: Device): Promise<string> {
-    const country = findCountryByCode(device.countryCode).country;
     const issuer = await this.evidentService.getIssuerByCountry(
-      device.organizationId,
-      country,
+      device.countryCode,
     );
 
-    if (issuer.data['hydra:member'].length > 0) {
-      return issuer.data['hydra:member'][0]['@id'];
+    if (!issuer) {
+      throw new Error(`No issuer found for country ${device.countryCode}`);
     }
 
-    return null;
+    return issuer.issuerId;
   }
 
   private async sendEmail(
